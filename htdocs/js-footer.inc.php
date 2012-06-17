d99b4d954b4c651014fb67af73b1e14c7b7a84b9
Keith Smiley <k@keithsmiley.net>
2012-06-16 23:52:59 -0400

    Factored out footer javascript.

---
+++ htdocs/js-footer.inc.php
@@ -0,0 +1,18 @@
+    <!--
+      E-mail address is obfuscated to (hopefully) deter spam. But if you’ve made
+      it this far, you probably know how to decipher it. Why not drop me a note?
+    -->
+    <script>
+      var c=':', u='k', h='keithsmiley.net', a='@', p='mailto',
+        e = document.getElementById("email");
+      e.attributes.href.value = p+c+u+a+h;
+      e.innerHTML = u+a+h;
+
+      var _gaq = [['_setAccount', 'UA-19711479-2'], ['_trackPageview']];
+      (function(d, t) {
+        var g = d.createElement(t),
+            s = d.getElementsByTagName(t)[0];
+        g.async = g.src = '//www.google-analytics.com/ga.js';
+        s.parentNode.insertBefore(g, s);
+      }(document, 'script'));
+    </script>
