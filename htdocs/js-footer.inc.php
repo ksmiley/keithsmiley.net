09590f646c475577122c764d523277c25147c5cc
Keith Smiley <k@keithsmiley.net>
2012-08-23 05:03:14 +0000

    Added JS to hide mobile address bar.

--- htdocs/js-footer.inc.php
+++ htdocs/js-footer.inc.php
@@ -3,10 +3,42 @@
       it this far, you probably know how to decipher it. Why not drop me a note?
     -->
     <script>
-      var c=':', u='k', h='keithsmiley.net', a='@', p='mailto',
-        e = document.getElementById("email");
-      e.attributes.href.value = p+c+u+a+h;
-      e.innerHTML = u+a+h;
+      (function(win) {
+        var doc = win.document,
+          c=':', u='k', h='keithsmiley.net', a='@', p='mailto',
+          e = doc.getElementById("email");
+        e.attributes.href.value = p+c+u+a+h;
+        e.innerHTML = u+a+h;
+
+        /*! Normalized address bar hiding for iOS & Android (c) @scottjehl MIT License */
+        /* Source: https://github.com/scottjehl/Hide-Address-Bar */
+        // If there's a hash, or addEventListener is undefined, stop here
+        if (!location.hash && win.addEventListener) {
+          // scroll to 1
+          window.scrollTo(0, 1);
+          var scrollTop = 1,
+            getScrollTop = function() {
+              return win.pageYOffset || doc.compatMode === "CSS1Compat" && doc.documentElement.scrollTop || doc.body.scrollTop || 0;
+            },
+            // reset to 0 on bodyready, if needed
+            bodycheck = setInterval(function() {
+              if (doc.body) {
+                clearInterval(bodycheck);
+                scrollTop = getScrollTop();
+                win.scrollTo(0, scrollTop === 1 ? 0 : 1);
+              }
+            }, 15);
+          win.addEventListener("load", function() {
+            setTimeout(function() {
+              // at load, if user hasn't scrolled more than 20 or so...
+              if (getScrollTop() < 20) {
+                // reset to hide addr bar at onload
+                win.scrollTo(0, scrollTop === 1 ? 0 : 1);
+              }
+            }, 0);
+          });
+        }
+      })(this);
 
       var _gaq = [['_setAccount', 'UA-19711479-2'], ['_trackPageview']];
       (function(d, t) {
